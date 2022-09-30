68ae8127eb9c3fd01b49b6afa70bf66c74e486be
Elisabeth <elisawbye@gmail.com>
2022-09-30 11:44:25 +0200

    Add ResponseState to handle API response

---
+++ app/src/main/java/com/elisawaa/comic/data/model/ResponseState.kt
@@ -0,0 +1,13 @@
+package com.elisawaa.comic.data.model
+
+sealed class ResponseState<out T> {
+
+    object Loading : ResponseState<Nothing>()
+
+    data class Success<T>(val data: T) : ResponseState<T>()
+
+    data class Error(
+        val t: Throwable
+    ) : ResponseState<Nothing>()
+
+}
