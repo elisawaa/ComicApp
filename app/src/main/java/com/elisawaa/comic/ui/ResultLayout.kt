990a62d68e7522afaecc686b62bf8937755b8065
Elisabeth <elisawbye@gmail.com>
2022-10-01 10:47:03 +0200

    Add loading, empty and error layouts

---
+++ app/src/main/java/com/elisawaa/comic/ui/ResultLayout.kt
@@ -0,0 +1,73 @@
+package com.elisawaa.comic.ui
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Error
+import androidx.compose.material.icons.filled.WbSunny
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun LoadingScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        CircularProgressIndicator()
+    }
+}
+
+@Composable
+fun EmptyScreen() {
+    ResultLayout(
+        icon = Icons.Default.WbSunny,
+        title = "Nothing here...",
+        description = "Nothing here yet."
+    )
+}
+
+@Composable
+fun ErrorScreen(errorMessage: String?) {
+    ResultLayout(
+        icon = Icons.Default.Error,
+        title = "Oops!",
+        description = errorMessage ?: "Something unexpected happened. Please try again!"
+    )
+}
+
+@Composable
+fun ResultLayout(icon: ImageVector, title: String, description: String) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(16.dp, Alignment.CenterVertically)
+    ) {
+        Icon(icon, null, tint = MaterialTheme.colorScheme.primary)
+        Text(
+            title,
+            style = MaterialTheme.typography.titleLarge,
+            color = MaterialTheme.colorScheme.onBackground,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            description,
+            style = MaterialTheme.typography.bodyMedium,
+            color = MaterialTheme.colorScheme.onBackground,
+            textAlign = TextAlign.Center
+        )
+    }
+}
