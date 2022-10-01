0250916a9a4c83d8e0c2bbda5bfb0fe65bce89d5
Elisabeth <elisawbye@gmail.com>
2022-10-01 19:45:42 +0200

    Add test tags

---
+++ app/src/main/java/com/elisawaa/comic/test/TestTags.kt
@@ -0,0 +1,11 @@
+package com.elisawaa.comic.test
+
+object TestTags {
+    const val TEST_TAG_COMIC_GRID_LIST = "ComicGridList"
+
+    const val TEST_TAG_BOTTOM_BAR_LIST = "BottomBarList"
+    const val TEST_TAG_BOTTOM_BAR_COMIC = "BottomBarComic"
+    const val TEST_TAG_BOTTOM_BAR_FAV = "BottomBarFav"
+
+    const val TEST_TAG_COMIC_FAB = "ComicFabTT"
+}
