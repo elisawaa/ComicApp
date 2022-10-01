2746f24bf5493822f1a6e1a0e8c031c4d8c2cf42
Elisabeth <elisawbye@gmail.com>
2022-10-01 10:48:29 +0200

    Add UI to Comic Screen, set up loading etc

--- app/src/main/java/com/elisawaa/comic/ui/comic/ComicScreen.kt
+++ app/src/main/java/com/elisawaa/comic/ui/comic/ComicScreen.kt
@@ -1,35 +1,61 @@
 package com.elisawaa.comic
 
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.FavoriteBorder
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import coil.compose.AsyncImage
-import coil.request.ImageRequest
+import com.elisawaa.comic.data.model.Comic
+import com.elisawaa.comic.ui.EmptyScreen
+import com.elisawaa.comic.ui.ErrorScreen
+import com.elisawaa.comic.ui.LoadingScreen
 import com.elisawaa.comic.ui.comic.ComicViewModel
+import com.elisawaa.comic.ui.comic.ZoomableImage
 
-@OptIn(ExperimentalMaterial3Api::class)
+
 @Composable
-fun ComicScreen(viewModel: ComicViewModel = hiltViewModel(), id: String? = null) {
+fun ComicScreen(viewModel: ComicViewModel = hiltViewModel()) {
     val state by viewModel.uiState.collectAsState()
+
+    if (state.loading) {
+        LoadingScreen()
+    }
+
+    if (state.error != null) {
+        ErrorScreen(state.error)
+    }
+
     state.comic?.let { comic ->
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-        ) {
+        ComicBody(comic)
+    }
+
+    if (!state.loading && state.error == null && state.comic == null) {
+        EmptyScreen()
+    }
+
+}
+
+@Composable
+fun ComicBody(comic: Comic) {
+    val scrollState = rememberScrollState()
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Box(modifier = Modifier.fillMaxSize()) {
             Column(
                 Modifier
                     .verticalScroll(rememberScrollState())
@@ -37,15 +63,16 @@ fun ComicScreen(viewModel: ComicViewModel = hiltViewModel(), id: String? = null)
                 verticalArrangement = Arrangement.spacedBy(16.dp),
             ) {
                 Text(
-                    comic.title,
+                    "#${comic.id} - ${comic.title}",
                     color = MaterialTheme.colorScheme.onBackground,
-                    style = MaterialTheme.typography.titleLarge
+                    style = MaterialTheme.typography.titleLarge,
+                    modifier = Modifier.align(Alignment.CenterHorizontally)
                 )
                 Text(
                     "Published: ${comic.day}/${comic.month}/${comic.year}",
                     color = MaterialTheme.colorScheme.onBackground
                 )
-                ComicImage(comic.img, comic.alt)
+                ComicImage(comic.img, comic.alt, scrollState)
 
                 Text(
                     "Transcript",
@@ -53,23 +80,40 @@ fun ComicScreen(viewModel: ComicViewModel = hiltViewModel(), id: String? = null)
                     style = MaterialTheme.typography.titleLarge
                 )
                 Text(comic.transcript, color = MaterialTheme.colorScheme.onBackground)
+                Button(
+                    onClick = { /*TODO*/ },
+                    modifier = Modifier.align(Alignment.CenterHorizontally)
+                ) {
+                    Text(
+                        "Random comic",
+                        color = MaterialTheme.colorScheme.onBackground
+                    )
+                }
+            }
+            FloatingActionButton(
+                onClick = {
+                    // TODO EWB
+                },
+                modifier = Modifier
+                    .align(Alignment.BottomEnd)
+                    .padding(end = 24.dp, bottom = 16.dp)
+            ) {
+                Icon(Icons.Outlined.FavoriteBorder, contentDescription = null)
             }
         }
     }
 }
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun ComicImage(imgUrl: String, contentDescription: String) {
-    AsyncImage(
-        model = ImageRequest.Builder(LocalContext.current)
-            .data(imgUrl)
-            .crossfade(true)
-            .build(),
-        placeholder = painterResource(R.drawable.ic_baseline_image_24),
+fun ComicImage(imgUrl: String, contentDescription: String, scrollState: ScrollState) {
+    ZoomableImage(
+        imgUrl = imgUrl,
         contentDescription = contentDescription,
         contentScale = ContentScale.FillWidth,
+        scrollState = scrollState,
         modifier = Modifier
             .clip(RoundedCornerShape(8.dp))
-            .fillMaxWidth()
+            .fillMaxSize()
     )
 }
